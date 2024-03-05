d8a5708e3f9a6d4ae72a9a676132a6f12410b8f7
Aditto002 <adittokoiri002@gmail.com>
2024-03-05 13:05:35 +0600

    redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,17 @@ import './index.css'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import router from './Route/Route.jsx'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { persistor, store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <PersistGate persistor={persistor} loading={null}>
     <RouterProvider router={router}>
      
     <App />
     </RouterProvider>
+    </PersistGate>
   </Provider>,
 )
